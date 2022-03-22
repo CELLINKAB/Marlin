16a1daa0c7490a97c7306f50f320a999cbfedf38
Skyelar Craver <sc@cellink.com>
2022-03-22 14:30:39 -0400

    add calculated scaling factors

--- Marlin/src/feature/pressure_sensor.cpp
+++ Marlin/src/feature/pressure_sensor.cpp
@@ -9,7 +9,12 @@
 
 void GcodeSuite::M1111()
 {
-    static auto report_fn = []{ SERIAL_ECHO_MSG("Pressure:", analogRead(PRESSURE_SENSOR_PIN)); };
+    static constexpr float KPA_SCALE_FACTOR = 4.1792;
+    static constexpr float KPA_ZERO_OFFSET = -689.11;
+    static constexpr auto pressure_scale = [](uint32_t analog_reading)
+    { return (static_cast<float>(analog_reading) * KPA_SCALE_FACTOR) - KPA_ZERO_OFFSET; };
+    static auto report_fn = []
+    { SERIAL_ECHO_MSG("Pressure (kPa):", pressure_scale(analogRead(PRESSURE_SENSOR_PIN))); };
     static IntervalReporter pressure_sensor(report_fn);
 
     report_fn();
