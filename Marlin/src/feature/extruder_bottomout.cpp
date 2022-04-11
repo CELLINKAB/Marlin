abba4e05169ad72a4945ac68bf322150a51d48fa
Skyelar Craver <sc@cellink.com>
2022-04-11 12:33:47 -0400

    fix busy infinite loop

--- Marlin/src/feature/extruder_bottomout.cpp
+++ Marlin/src/feature/extruder_bottomout.cpp
@@ -7,6 +7,7 @@
 #include "../gcode/gcode.h"
 #include "../module/planner.h"
 #include "../module/stepper/trinamic.h"
+#include "../module/motion.h"
 
 #include "interval_reporter.h"
 
@@ -16,17 +17,9 @@ void GcodeSuite::G511()
     static auto init_ = []
     {
         tmc_enable_stallguard(stepperE0);
-        SET_INPUT_PULLDOWN(E0_STOP_PIN);
+        SET_INPUT_PULLUP(E0_STOP_PIN);
         return 0;
     }();
-
-    if (parser.boolval('S'))
-    {
-        static auto check_stop_pin = IntervalReporter{[]
-                                                      { SERIAL_ECHOPGM("E_STOP:", READ(E0_STOP_PIN), ", SG_result:", stepperE0.SG_RESULT()); }};
-        check_stop_pin.start();
-        return;
-    }
 
     if (READ(E0_STOP_PIN))
     {
@@ -36,22 +29,19 @@ void GcodeSuite::G511()
     }
 
     bool stall_triggered = false;
-    attachInterrupt(E0_STOP_PIN, callback_function_t{[&stall_triggered]()
-                                                     {
-                                                         stall_triggered = true;
-                                                         planner.quick_stop();
-                                                         sync_plan_position_e();
-                                                        planner.synchronize();
-                                                     }},
-                    RISING);
+    attachInterrupt(
+        E0_STOP_PIN,
+        [&stall_triggered]
+        { stall_triggered = true; },
+        RISING);
 
     auto end_position = current_position.copy();
-    end_position.e += 5000.0f;
-    planner.buffer_segment(end_position, parser.feedrateval('F', 1.0f));
-    while (!stall_triggered)
-    {
-        delay(100);
-    }
+    end_position.e += 3000.0f;
+    planner.buffer_segment(end_position, parser.feedrateval('F', 10.0f));
+    const auto timeout = millis() + 3000;
+    while (!stall_triggered && millis() < timeout)
+        idle();
+    quickstop_stepper();
     detachInterrupt(E0_STOP_PIN);
 }
 #endif
