906870dd753dece156ef49dc10b8318a2ffe313f
Skyelar Craver <sc@cellink.com>
2022-05-26 12:20:15 -0400

    move expected probe location down to accomodate new probe

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1210,7 +1210,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 140, 67, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { 140, 67, -5 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
