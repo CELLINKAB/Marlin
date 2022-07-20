cd824b1f6ab18818456b98e4b67208ff61b4b433
Skyelar Craver <sc@cellink.com>
2022-07-20 13:33:49 -0400

    add Gcode to adjust stepper probe parameters

--- Marlin/src/feature/stepper_retracting_probe.cpp
+++ Marlin/src/feature/stepper_retracting_probe.cpp
@@ -4,8 +4,20 @@
 
 #if ENABLED(STEPPER_RETRACTING_PROBE)
 
+#include "../gcode/gcode.h"
+#include "../gcode/parser.h"
 #include "stepper_retracting_probe.h"
 
 StepperRetractingProbe stepper_probe;
+
+void GcodeSuite::M1029() {
+    StepperRetractingProbe::Config new_conf = stepper_probe.get_config();
+    if (parser.seen('T')) new_conf.stall_threshold = parser.value_byte();
+    if (parser.seen('C')) new_conf.stepper_current = parser.value_int();
+    if (parser.seen('D')) new_conf.deploy_velocity = parser.value_int();
+    if (parser.seen('S')) new_conf.stow_velocity = parser.value_int();
+    if (parser.seen('B')) new_conf.minimum_retract_time = parser.value_int();
+    stepper_probe.set_config(new_conf);
+}
 
 #endif
