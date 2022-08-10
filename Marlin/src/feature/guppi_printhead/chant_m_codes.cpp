8efaf436d08a6e539c802a8f14121fedfbf2dd1a
Skyelar Craver <sc@cellink.com>
2022-08-10 12:10:06 -0400

    add printhead M code declarations from torsk

---
+++ Marlin/src/feature/guppi_printhead/chant_m_codes.cpp
@@ -0,0 +1,208 @@
+// copyright Cellink 2022 GPLv3
+
+#include "../../gcode/gcode.h"
+#include "../../gcode/parser.h"
+#include "request.h"
+
+//StartActuatingPrinthead
+void GcodeSuite::M750() {}
+//StopActuatingPrinthead
+void GcodeSuite::M751() {}
+//StartActuatingCoaxial
+void GcodeSuite::M752() {}
+//StopActuatingCoaxial
+void GcodeSuite::M753() {}
+//EnumeratePrintheads
+void GcodeSuite::M770() {}
+//SetCurrentPrintheadTemp
+void GcodeSuite::M771() {}
+//GetAllPrintheadsTemps
+void GcodeSuite::M772() {}
+//SetPrintheadPressure
+void GcodeSuite::M773() {}
+//GetPrintheadPressure
+void GcodeSuite::M774() {}
+//SetPrintHdHeaterPIDparams
+void GcodeSuite::M777() {}
+//GetPrintHdHeaterPIDparams
+void GcodeSuite::M778() {}
+//SetPrintheadTempFilterParams
+void GcodeSuite::M779() {}
+//GetPrintheadTempFilterParams
+void GcodeSuite::M780() {}
+//SetPrintHdHeaterMaxPow
+void GcodeSuite::M781() {}
+//GetPrintHeadProbedStat
+void GcodeSuite::M782() {}
+//GetPrintheadType
+void GcodeSuite::M783() {}
+//GetPrintheadMounted
+void GcodeSuite::M784() {}
+//GetPrintheadUUID
+void GcodeSuite::M785() {}
+//SetPrintheadLED
+void GcodeSuite::M786() {}
+//GetPrintheadLEDDetails
+void GcodeSuite::M787() {}
+//SetPrintheadRoomTemp
+void GcodeSuite::M788() {}
+//SetPrintheadFeedrateMultiplier
+void GcodeSuite::M790() {}
+//GetPrintheadFeedrateMultiplier
+void GcodeSuite::M791() {}
+//SetPrintheadFanSpeed
+void GcodeSuite::M792() {}
+//GetPrintheadFanSpeed
+void GcodeSuite::M793() {}
+//SetPHAmpCorrParams
+void GcodeSuite::M794() {}
+//GetPHAmpCorrParams
+void GcodeSuite::M795() {}
+//GetPHSWVersion
+void GcodeSuite::M796() {}
+//SetPHNotCalibrated
+void GcodeSuite::M797() {}
+//SetPrintheadExternalPWM
+void GcodeSuite::M798() {}
+//GetPrintheadExternalPWM
+void GcodeSuite::M799() {}
+//DisableBedTempController
+void GcodeSuite::M800() {}
+//SetBedTempController
+void GcodeSuite::M801() {}
+//GetBedTempControllerInfo
+void GcodeSuite::M802() {}
+//SetPeltierOutputLimit
+void GcodeSuite::M803() {}
+//SetThermistorUnifiedParams
+void GcodeSuite::M804() {}
+//SetUVCrosslinkingLEDs
+void GcodeSuite::M805() {}
+//SetUVSterilization
+void GcodeSuite::M806() {}
+//SetCleanchamberFan
+void GcodeSuite::M807() {}
+//SetAirValves
+void GcodeSuite::M808() {}
+//ControlRGBLED
+void GcodeSuite::M810() {}
+//ControlPHVerticalMove
+void GcodeSuite::M811() {}
+//GetZ3EndstopStatus
+void GcodeSuite::M814() {}
+//GetTankNRegulatorPressure
+void GcodeSuite::M816() {}
+//MovePHsVertically
+void GcodeSuite::M817() {}
+//ReadDoorStatus
+void GcodeSuite::M818() {}
+//SetBedCoolingFans
+void GcodeSuite::M819() {}
+//PrinterHomingStatus
+void GcodeSuite::M821() {}
+//ControlAirPumpStatus
+void GcodeSuite::M822() {}
+//SafePark
+void GcodeSuite::M823() {}
+//GetCurrActiveTool
+void GcodeSuite::M824() {}
+//GetCurrPhotocuringParams
+void GcodeSuite::M825() {}
+//SetHomeDirection
+void GcodeSuite::M826() {}
+//SetDarkMode
+void GcodeSuite::M830() {}
+//SetMotorCurrent
+void GcodeSuite::M842() {}
+//ControlPin
+void GcodeSuite::M848() {}
+//GetSystemTime
+void GcodeSuite::M849() {}
+//GetToolMachineOffset
+void GcodeSuite::M855() {}
+//ProbeSurface
+void GcodeSuite::M856() {}
+//SetZProbePlanePosition
+void GcodeSuite::M857() {}
+//GetIsAutoBedLevelingPointsSet
+void GcodeSuite::M858() {}
+//PrintToolOffset
+void GcodeSuite::M860() {}
+//SetDropSegments
+// void GcodeSuite::M900() {} CONFLICT - linear advance
+//DigitalTrimpotControl
+void GcodeSuite::M908() {}
+//RetrieveHardwareRevision
+void GcodeSuite::M910() {}
+//GetPHHWConnectionStatus
+void GcodeSuite::M1001() {}
+//DbgTrig
+void GcodeSuite::M1002() {}
+//ControlPHActuateStatus
+void GcodeSuite::M1004() {}
+//GetPHMountedStatus
+void GcodeSuite::M1005() {}
+//SendCustomCommandToPH
+void GcodeSuite::M1006() {}
+//ResetAwaitingResponse
+void GcodeSuite::M1008() {}
+//SetPrintheadHeaterValue
+void GcodeSuite::M1012() {}
+//PrintCurrentPosition
+void GcodeSuite::M1015() {}
+//PrintCurrentMechanicalPos
+void GcodeSuite::M1016() {}
+//PrintCurrentToolOffset
+void GcodeSuite::M1017() {}
+//DbgPhCom
+void GcodeSuite::M1018() {}
+//ReadExternalGPIOs
+void GcodeSuite::M1020() {}
+//GetAllPHTempStatus
+void GcodeSuite::M1023() {}
+//ActivateToolDBG
+void GcodeSuite::M1024() {}
+//BedTempDebug
+void GcodeSuite::M1025() {}
+//SetPeltierValue
+void GcodeSuite::M1028() {}
+//SetBedTempContrlParams
+void GcodeSuite::M1034() {}
+//SetBedThermisterParams
+void GcodeSuite::M1035() {}
+//SetRegulatorPressure
+void GcodeSuite::M1036() {}
+//GetAllAxesMotorCurrent
+void GcodeSuite::M1037() {}
+//GetPrintbedProbesStatus
+void GcodeSuite::M1038() {}
+//SetPeltierPolarity
+void GcodeSuite::M1039() {}
+//EnableDisableBuck
+void GcodeSuite::M1040() {}
+//StartStopPump
+void GcodeSuite::M1042() {}
+//GetAllPositions
+void GcodeSuite::M1045() {}
+//GetAllToolOffsets
+void GcodeSuite::M1046() {}
+//EnableDisableHeartbeat
+void GcodeSuite::M1047() {}
+//GetPrintbedPeltierConfig
+void GcodeSuite::M1048() {}
+//ChangeSerialBaudrate
+void GcodeSuite::M1050() {}
+//CheckCurrentCompileVersion
+void GcodeSuite::M1051() {}
+//GetPressureAndExtrusionData
+void GcodeSuite::M1062() {}
+//MovePrintheadsUpDownDBG
+void GcodeSuite::M1063() {}
+//IgnoreCurrentPrinthead
+void GcodeSuite::M1064() {}
+//SetPrintheadsTemperatureVerboseMode
+void GcodeSuite::M1065() {}
+//SetPhotocuringModuleVerboseMode
+void GcodeSuite::M1066() {}
+//DBGRetrieveCurrentExtADC
+void GcodeSuite::M1070() {}
