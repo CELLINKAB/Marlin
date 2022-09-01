437bcd3b045f227b25db9250923614232cacc7bc
Skyelar Craver <sc@cellink.com>
2022-09-01 11:31:07 -0400

    Use more precise extruder steps per mm

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -937,7 +937,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 320, 320, 1280, 21023}
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 320, 320, 1280, 21333.333}
 
 /**
  * Default Max Feed Rate (mm/s)
