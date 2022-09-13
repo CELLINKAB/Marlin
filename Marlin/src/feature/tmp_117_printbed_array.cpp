7eaff01c7baf10b107127e950688809fa1c14af7
Skyelar Craver <sc@cellink.com>
2022-09-13 16:43:08 -0400

    HOTFIX: fix initialization order (again)

--- Marlin/src/feature/tmp_117_printbed_array.cpp
+++ Marlin/src/feature/tmp_117_printbed_array.cpp
@@ -11,32 +11,36 @@
 
 #    include "tmp117_printbed.h"
 
-static std::array<TMP117<TwoWire>, 4> bed_sensors{[]() {
-    TMP117 sensor_1(TMPAddr::GND);
-    sensor_1.init(nullptr);
-    TMP117 sensor_2(TMPAddr::SCL);
-    sensor_2.init(nullptr);
-    TMP117 sensor_3(TMPAddr::SDA);
-    sensor_3.init(nullptr);
-    TMP117 sensor_4(TMPAddr::VCC);
-    sensor_4.init(nullptr);
-    return std::array{sensor_1, sensor_2, sensor_3, sensor_4};
-}()};
+static std::array<TMP117<TwoWire>, 4>& bed_sensors()
+{
+    static std::array<TMP117<TwoWire>, 4> sensors{[]() {
+        TMP117 sensor_1(TMPAddr::GND);
+        sensor_1.init(nullptr);
+        TMP117 sensor_2(TMPAddr::SCL);
+        sensor_2.init(nullptr);
+        TMP117 sensor_3(TMPAddr::SDA);
+        sensor_3.init(nullptr);
+        TMP117 sensor_4(TMPAddr::VCC);
+        sensor_4.init(nullptr);
+        return std::array{sensor_1, sensor_2, sensor_3, sensor_4};
+    }()};
+    return sensors;
+}
 
 double get_tmp117_bed_temp()
 {
     double total_temps = 0.0;
-    for (auto& sensor : bed_sensors) {
+    for (auto& sensor : bed_sensors()) {
         total_temps += sensor.getTemperature();
     }
-    const double avg = total_temps / bed_sensors.size();
+    const double avg = total_temps / bed_sensors().size();
     return avg;
 }
 
 void GcodeSuite::M802()
 {
     uint8_t sensor_num = 0;
-    for (auto& sensor : bed_sensors) {
+    for (auto& sensor : bed_sensors()) {
         SERIAL_ECHO("bed_temperature_");
         SERIAL_ECHO(sensor_num++);
         SERIAL_CHAR(':');
