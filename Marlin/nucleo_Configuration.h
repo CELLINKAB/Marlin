245fdeaf75d42a329eea64df52ba32694408ca73
Skyelar Craver <sc@cellink.com>
2022-10-24 16:10:28 -0400

    update tempperature config

--- Marlin/nucleo_Configuration.h
+++ Marlin/nucleo_Configuration.h
@@ -577,7 +577,7 @@
 #define HEATER_5_MAXTEMP 275
 #define HEATER_6_MAXTEMP 275
 #define HEATER_7_MAXTEMP 275
-#define BED_MAXTEMP      150
+#define BED_MAXTEMP      60
 #define CHAMBER_MAXTEMP  60
 
 /**
@@ -637,7 +637,7 @@
  * heater. If your configuration is significantly different than this and you don't understand
  * the issues involved, don't use bed PID until someone else verifies that your hardware works.
  */
-#define PIDTEMPBED
+//#define PIDTEMPBED
 
 //#define BED_LIMIT_SWITCHING
 
