98dc18fb7828d7f5fd0e38fe2ede04acc5082949
Skyelar Craver <sc@cellink.com>
2022-10-28 16:27:31 -0400

    move functions

---
+++ Marlin/src/feature/festo_pneumatics.cpp
@@ -0,0 +1,101 @@
+
+
+#if ENABLED(FESTO_PNEUMATICS)
+
+#    include "festo_pneumatics.h"
+
+#    include "../module/planner.h"
+
+namespace pneumatics {
+
+//
+// Pressure Regulation
+//
+
+//
+// Mixing
+//
+
+constexpr static pin_t get_valve(uint8_t tool)
+{
+    switch (tool) {
+        case 0: return PRESSURE_VALVE_C1_PIN;
+        case 1: return PRESSURE_VALVE_C2_PIN;
+        case 2: return PRESSURE_VALVE_C3_PIN;
+        default: return NC;
+    }
+}
+
+void apply_mixing_pressure(uint8_t tool)
+{
+    const pin_t pin = get_valve(tool);
+    planner.synchronize();
+    WRITE(pin, !PRESSURE_VALVE_CLOSE_LEVEL);
+    delay(100);
+}
+
+void release_mixing_pressure(uint8_t tool)
+{
+    const pin_t pin = get_valve(tool);
+    planner.synchronize();
+    WRITE(pin, PRESSURE_VALVE_CLOSE_LEVEL);
+    delay(100);
+}
+
+//
+// Lid Gripper
+//
+
+void gripper_release()
+{
+    static constexpr LID_GRIPPER_RELEASE_LEVEL = LID_GRIPPER_NORMALLY_CLOSED ? HIGH : LOW;
+    #if ENABLED(CHECK_LID_GRIPPER_LOCATION_BEFORE_RELEASE)
+        static constexpr xyz_pos_t safe_lid_drop_pos = LID_GRIPPER_RELEASE_LOCATION;
+        if (current_position != safe_lid_drop_pos) {
+            SERIAL_ECHOLN("printbed in wrong location for release!");
+            return;
+        }
+    #endif
+    WRITE(LID_GRIPPER_VACUUM_PIN, !PRESSURE_VALVE_CLOSE_LEVEL);
+    delay(100);
+    WRITE(LID_GRIPPER_VACUUM_PIN, PRESSURE_VALVE_CLOSE_LEVEL);
+}
+
+//
+// Analog Pressure Sensor
+//
+
+AnalogPressureSensor::AnalogPressureSensor(pin_t sense_pin, float scale_factor, float offset_kPa)
+    : scalar(scale_factor)
+    , offset(offset_kPa)
+    , pin(sense_pin)
+{
+    pinMode(sense_pin, INPUT_ANALOG);
+}
+
+float AnalogPressureSensor::read_avg(bool with_scaling = true, bool with_offset = true) const
+{
+    uint32_t samples[40]{};
+    for (uint32_t& sample : samples) {
+        sample = read_raw();
+        delay(1);
+    }
+    float avg_raw = std::accumulate(std::cbegin(samples), std::cend(samples), 0.0f) / 40.0f;
+    return apply_scaling_leveling(avg_raw, with_scaling, with_offset);
+}
+
+float AnalogPressureSensor::apply_scaling_leveling(float reading,
+                                                   bool with_scaling,
+                                                   bool with_offset) const
+{
+    return (reading * (static_cast<float>(with_scaling) * scalar))
+           - (static_cast<float>(with_offset) * offset);
+}
+
+AnalogPressureSensor gripper_vacuum(GRIPPER_VACUUM_PIN);
+AnalogPressureSensor tank_pressure(PRESSURE_TANK_PIN);
+AnalogPressureSensor regulator_feedback(PRESSURE_REGULATOR_SENSE_PIN);
+
+} // namespace pneumatics
+
+#endif // FESTO_PNEUMATICS
---
+++ Marlin/src/feature/festo_pneumatics.h
@@ -0,0 +1,63 @@
+
+#pragma once
+
+#include "../inc/MarlinConfigPre.h"
+
+#include <numeric>
+
+namespace pneumatics {
+
+#ifndef PRESSURE_VALVE_CLOSE_LEVEL
+  #define PRESSURE_VALVE_CLOSE_LEVEL LOW
+#endif
+
+void gripper_release();
+
+void apply_mixing_pressure(uint8_t tool);
+void release_mixing_pressure(uint8_t tool);
+
+struct AnalogPressureSensor
+{
+    AnalogPressureSensor(pin_t sense_pin, float scale_factor = 1.0f, float offset_kPa = 0.0f);
+
+    float scalar;
+    float offset;
+
+    /**
+     * @brief read the sensor and return raw ADC output
+     * 
+     * @return uint32_t 
+     */
+    inline uint32_t read_raw() const { return analogRead(pin); } // not portable
+
+    inline float read_volts() const
+    {
+        return (ADC_VREF / static_cast<float>(1 << ADC_RESOLUTION))
+               * static_cast<float>(analogRead(pin));
+    }
+
+    /**
+     * @brief read the sensor and return scaled kPa reading
+     * 
+     * @return float 
+     */
+    inline float read(bool with_scaling = true, bool with_offset = true) const
+    {
+        return apply_scaling_leveling(static_cast<float>(read_raw()), with_scaling, with_offset);
+    }
+
+    float read_avg(bool with_scaling = true, bool with_offset = true) const;
+
+    inline void tare() { offset = read_avg(true, false); }
+
+private:
+    pin_t pin;
+
+    float apply_scaling_leveling(float reading, bool with_scaling, bool with_offset) const;
+}; // AnalogPressureSensor
+
+extern AnalogPressureSensor gripper_vacuum;
+extern AnalogPressureSensor tank_pressure;
+extern AnalogPressureSensor regulator_feedback;
+
+} // namespace pneumatics
