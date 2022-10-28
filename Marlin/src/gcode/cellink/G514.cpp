2b688e1d2a4f22e9fa71bd29826c255d57cab993
Skyelar Craver <sc@cellink.com>
2022-10-28 16:27:00 -0400

    move functions

---
+++ Marlin/src/gcode/cellink/G514.cpp
@@ -0,0 +1,23 @@
+
+#include "../../inc/MarlinConfig.h"
+
+#if ENABLED(FESTO_PNEUMATICS)
+
+#    include "../../feature/festo_pneumatics.h"
+#    include "../gcode.h"
+#    include "../parser.h"
+
+/**
+ * @brief mixing extrude / pneumatic move
+ * 
+ */
+void GcodeSuite::G514()
+{
+    const auto tool = get_active_tool();
+    planner.synchronize();
+    pneumatics::apply_mixing_pressure(tool);
+    G0_G1();
+    pneumatics::release_mixing_pressure(tool);
+}
+
+#endif // FESTO_PNEUMATICS
