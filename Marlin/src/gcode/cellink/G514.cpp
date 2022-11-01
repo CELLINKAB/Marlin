474c7b15ea5fe692e527711e6385d29736f17170
Skyelar Craver <sc@cellink.com>
2022-11-01 16:18:52 -0400

    add gripper Gcode

--- Marlin/src/gcode/cellink/G514.cpp
+++ Marlin/src/gcode/cellink/G514.cpp
@@ -13,11 +13,15 @@
  */
 void GcodeSuite::G514()
 {
-    const auto tool = get_active_tool();
-    planner.synchronize();
+    const auto tool = get_target_extruder_from_command();
     pneumatics::apply_mixing_pressure(tool);
     G0_G1();
     pneumatics::release_mixing_pressure(tool);
+}
+
+void GcodeSuite::G515()
+{
+    pneumatics::gripper_release();
 }
 
 #endif // FESTO_PNEUMATICS
