117bdced7a78ec58b06d41ed701492a8fd4cdd3a
Skyelar Craver <sc@cellink.com>
2022-11-01 16:19:18 -0400

    add empty pressure regulator codes

---
+++ Marlin/src/gcode/cellink/M1036_M1062.cpp
@@ -0,0 +1,17 @@
+
+#include "../../inc/MarlinConfig.h"
+
+#if ENABLED(FESTO_PNEUMATICS)
+
+#include "../gcode.h"
+#include "../../feature/festo_pneumatics.h"
+
+// set pressure regulator
+void GcodeSuite::M1036() {
+    
+}
+
+
+void GcodeSuite::M1062() {}
+
+#endif  // FESTO_PNEUMATICS
