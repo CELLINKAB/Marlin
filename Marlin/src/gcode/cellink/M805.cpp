42aabcc8e0733044cc6651ee5166bbae6a1dfa1f
Skyelar Craver <sc@cellink.com>
2022-11-07 15:33:21 -0500

    add photocuring implementation

--- Marlin/src/gcode/cellink/M805.cpp
+++ Marlin/src/gcode/cellink/M805.cpp
@@ -10,10 +10,23 @@
 
 #include "../../feature/simple_TMC_controller.h"
 
-constexpr uint32_t steps_for_wavelength(uint16_t wavelength)
+struct CuringLed
 {
-    case 4:
+    pin_t pin;
+    uint32_t steps;
+};
+
+constexpr CuringLed led_for_wavelength(uint16_t wavelength)
+{
+    switch (wavelength)
+    case 365: return CuringLed{UV_365_PIN, 100};
+    case 400: return CuringLed{UV_400_PIN, 200};
+    case 480: return CuringLed{UV_480_PIN, 300};
+    case 520: return CuringLed{UV_520_PIN, 400};
+    default: return CuringLed{NC, 0};
 }
+
+constexpr pin_t 
 
 GcodeSuite::M805()
 {
@@ -24,7 +37,14 @@ GcodeSuite::M805()
     const uint8_t intensity = parser.byteval('I');
     const uint16_t wavelength = parser.ushortval('W');
     const millis_t duration = ((parser.ushortval('S') * 1000) + parser.ushortval('P'));
-    
+
+    const CuringLed led = led_for_wavelength(wavelength);
+
+    stepper.move(led.steps);
+    millis_t end_time = millis() + duration;
+    pwm_start(led.pin, intensity, PERCENT_COMPARE_FORMAT);
+    while (millis() < duration) idle();
+    pwm_stop(led.pin);
 }
 
 #endif // UV_CROSSLINKING
