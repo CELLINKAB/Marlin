712f466b20d57ec0a4172460bbc15e910567fcad
Skyelar Craver <sc@cellink.com>
2022-11-08 13:29:33 -0500

    add homing, start making move function

--- Marlin/src/gcode/cellink/M805.cpp
+++ Marlin/src/gcode/cellink/M805.cpp
@@ -30,6 +30,37 @@ constexpr CuringLed led_for_wavelength(uint16_t wavelength)
     }
 }
 
+void single_step()
+{
+    static uint32_t idle_counter = 0;
+    WRITE(PC_STEP_PIN, HIGH);
+    delayMicroseconds(1);
+    WRITE(PC_STEP_PIN, LOW);
+    delayMicroseconds(1);
+    idle_counter += 1;
+    if (idle_counter >= 10000) {
+        idle();
+        idle_counter = 0;
+    }
+
+}
+
+void move_rainbow(CuringLed led)
+{
+    static constexpr uint8_t stop_dir = LOW;
+    static uint32_t rainbow_position = []{
+        OUT_WRITE(PC_DIR_PIN, stop_dir);
+        OUT_WRITE(PC_STEP_PIN, LOW);
+        uint32_t max_steps = 100'000;
+        while ( max_steps-- && READ(PC_STOP_PIN) == LOW) {
+            single_step();
+        }
+        return 0;
+    }();
+    if (led.steps == rainbow_position) return;
+    //if (led.steps < rainbow_position)
+}
+
 void GcodeSuite::M805()
 {
     static auto stepper = [] {
