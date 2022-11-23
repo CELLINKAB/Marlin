59e813644459baf50ee7e8428d73a827b637d5eb
Skyelar Craver <sc@cellink.com>
2022-11-23 09:32:29 -0500

    disable freeze pin
    Signed-off-by: Skyelar Craver <sc@cellink.com>

--- Marlin/myco_Configuration_adv.h
+++ Marlin/myco_Configuration_adv.h
@@ -4138,7 +4138,7 @@
  * Instant freeze / unfreeze functionality
  * Potentially useful for emergency stop that allows being resumed.
  */
-#define FREEZE_FEATURE
+//#define FREEZE_FEATURE
 #if ENABLED(FREEZE_FEATURE)
   //#define FREEZE_PIN 41   // Override the default (KILL) pin here
   #define FREEZE_STATE LOW  // State of pin indicating freeze
