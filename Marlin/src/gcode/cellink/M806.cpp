ec80a82cc124392e5c043995c8feebcba50b8a2c
Skyelar Craver <sc@cellink.com>
2022-12-02 16:44:36 -0500

    change logic for DC condition
    Signed-off-by: Skyelar Craver <sc@cellink.com>

--- Marlin/src/gcode/cellink/M806.cpp
+++ Marlin/src/gcode/cellink/M806.cpp
@@ -19,7 +19,7 @@ void GcodeSuite::M806()
     const auto exposure_seconds = min(parser.ulongval('S', 600), 1200UL);
     const auto frequency = min(parser.ulongval('F', 1000), 2000UL);
     const bool safety_override = parser.boolval('O');
-    
+
     const millis_t end_time = millis() + SEC_TO_MS(exposure_seconds);
 
     const uint32_t us_per_period = 1'000'000 / frequency;
@@ -33,18 +33,22 @@ void GcodeSuite::M806()
     };
 
     WRITE(UVC_RELAY_PIN, HIGH);
+    //TODO: this logic seems messy
     while ((safety_override || (READ(UVC_TFAULT_PIN) == LOW)) && millis() < end_time) {
-        WRITE(UVC_PWM_PIN, HIGH);
+        if (intensity > 0)
+            WRITE(UVC_PWM_PIN, HIGH);
         if (intensity >= 128)
             time_fixed_idle(us_on_time);
         else
             delayMicroseconds(us_on_time);
-        WRITE(UVC_PWM_PIN, LOW);
+        if (intensity < 255)
+            WRITE(UVC_PWM_PIN, LOW);
         if (intensity < 128)
             time_fixed_idle(us_off_time);
         else
             delayMicroseconds(us_off_time);
     }
+    WRITE(UVC_PWM_PIN, LOW);
     WRITE(UVC_RELAY_PIN, LOW);
 }
 
