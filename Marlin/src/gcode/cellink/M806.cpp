bd5a8706c4b22a46f50048f367dc9e5d059d4b82
Skyelar Craver <sc@cellink.com>
2022-12-02 16:24:18 -0500

    cleanup types, add safety override
    Signed-off-by: Skyelar Craver <sc@cellink.com>

--- Marlin/src/gcode/cellink/M806.cpp
+++ Marlin/src/gcode/cellink/M806.cpp
@@ -3,8 +3,8 @@
 
 #if ENABLED(UVC_STERILIZATION)
 
+#    include "../../MarlinCore.h"
 #    include "../gcode.h"
-#include "../../MarlinCore.h"
 
 void GcodeSuite::M806()
 {
@@ -14,25 +14,36 @@ void GcodeSuite::M806()
         SET_INPUT_PULLDOWN(UVC_TFAULT_PIN);
         return true;
     }();
-    
+
     const auto intensity = constrain(parser.byteval('I', 255), 0, 255);
     const auto exposure_seconds = max(parser.ulongval('S', 600), 1200UL);
     const auto frequency = min(parser.ulongval('F', 1000), 2000UL);
-    const auto end_time = millis() + (exposure_seconds * 1000);
+    const bool safety_override = parser.boolval('O');
+    
+    const millis_t end_time = millis() + SEC_TO_MS(exposure_seconds);
 
-    uint32_t us_per_period = 1'000'000 / frequency;
-    uint32_t us_on_time = us_per_period / 255 * intensity;
-    uint32_t us_off_time = us_per_period - us_on_time;
+    const uint32_t us_per_period = 1'000'000 / frequency;
+    const uint32_t us_on_time = us_per_period / 255 * intensity;
+    const uint32_t us_off_time = us_per_period - us_on_time;
 
-    auto time_fixed_idle = [](uint32_t micro_seconds){uint32_t turn_off_time = micros() + micro_seconds; idle(); delayMicroseconds(turn_off_time - micros());}; 
+    const auto time_fixed_idle = [](uint32_t micro_seconds) {
+        uint32_t turn_off_time = micros() + micro_seconds;
+        idle();
+        delayMicroseconds(turn_off_time - micros());
+    };
 
     WRITE(UVC_RELAY_PIN, HIGH);
-    while (/*(READ(UVC_ALARM_PIN) == LOW) &&*/ millis() < end_time)
-    {
-        WRITE(UVC_PWM_PIN, HIGH)   ;
-        if (intensity >= 128) time_fixed_idle(us_on_time); else delayMicroseconds(us_on_time);
+    while ((safety_override || (READ(UVC_TFAULT_PIN) == LOW)) && millis() < end_time) {
+        WRITE(UVC_PWM_PIN, HIGH);
+        if (intensity >= 128)
+            time_fixed_idle(us_on_time);
+        else
+            delayMicroseconds(us_on_time);
         WRITE(UVC_PWM_PIN, LOW);
-        if (intensity < 128) time_fixed_idle(us_off_time); else delayMicroseconds(us_off_time);
+        if (intensity < 128)
+            time_fixed_idle(us_off_time);
+        else
+            delayMicroseconds(us_off_time);
     }
     WRITE(UVC_RELAY_PIN, LOW);
 }
