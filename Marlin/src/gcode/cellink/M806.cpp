76f4efd2283dfa0db11fb7634efb567d983058b6
Skyelar Craver <sc@cellink.com>
2022-12-02 16:06:25 -0500

    cheeky software PWM
    Signed-off-by: Skyelar Craver <sc@cellink.com>

--- Marlin/src/gcode/cellink/M806.cpp
+++ Marlin/src/gcode/cellink/M806.cpp
@@ -15,19 +15,25 @@ void GcodeSuite::M806()
         return true;
     }();
     
-    const auto intensity = parser.byteval('I', 255);
-    const auto exposure_seconds = parser.ushortval('S', 600);
-    const auto frequency = parser.ulongval('F', 1000);
+    const auto intensity = constrain(parser.byteval('I', 255), 0, 255);
+    const auto exposure_seconds = max(parser.ulongval('S', 600), 1200UL);
+    const auto frequency = min(parser.ulongval('F', 1000), 2000UL);
     const auto end_time = millis() + (exposure_seconds * 1000);
 
+    uint32_t us_per_period = 1'000'000 / frequency;
+    uint32_t us_on_time = us_per_period / 255 * intensity;
+    uint32_t us_off_time = us_per_period - us_on_time;
+
+    auto time_fixed_idle = [](uint32_t micro_seconds){uint32_t turn_off_time = micros() + micro_seconds; idle(); delayMicroseconds(turn_off_time - micros());}; 
+
     WRITE(UVC_RELAY_PIN, HIGH);
-    analogWrite(UVC_PWM_PIN, intensity);
     while (/*(READ(UVC_ALARM_PIN) == LOW) &&*/ millis() < end_time)
     {
-        delay(100);
-        idle();
+        WRITE(UVC_PWM_PIN, HIGH)   ;
+        if (intensity >= 128) time_fixed_idle(us_on_time); else delayMicroseconds(us_on_time);
+        WRITE(UVC_PWM_PIN, LOW);
+        if (intensity < 128) time_fixed_idle(us_off_time); else delayMicroseconds(us_off_time);
     }
-    analogWrite(UVC_PWM_PIN, 0);
     WRITE(UVC_RELAY_PIN, LOW);
 }
 
