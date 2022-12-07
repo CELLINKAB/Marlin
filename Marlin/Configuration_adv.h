86c83417e82c327d00d0e503217a1bf7f8cdd4d3
Skyelar Craver <sc@cellink.com>
2022-12-07 01:45:28 -0500

    simpler config switching
    Signed-off-by: Skyelar Craver <sc@cellink.com>

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1,9 +1,6 @@
 
 #pragma once
 
-// TODO: find a cleaner way or simplify to one file
-// #define NUCLEO_BUILD
-#define MYCO_BUILD
 #ifdef NUCLEO_BUILD
   #include "nucleo_Configuration_adv.h"
 #elif defined(MYCO_BUILD)
