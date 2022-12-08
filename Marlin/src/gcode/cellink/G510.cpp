c824b8cabc047dcf605c7c7b6b9659a537057880
Skyelar Craver <sc@cellink.com>
2022-12-08 17:43:48 -0500

    update constants
    Signed-off-by: Skyelar Craver <sc@cellink.com>

--- Marlin/src/gcode/cellink/G510.cpp
+++ Marlin/src/gcode/cellink/G510.cpp
@@ -9,7 +9,7 @@
 void GcodeSuite::G510()
 {
   const auto feedrate = parser.feedrateval('F', AUTOCAL_DEFAULT_FEEDRATE);
-  if (optical_autocal.full_autocal_routine(MMM_TO_MMS(feedrate)))
+  if (optical_autocal.full_autocal_routine(feedrate))
   {
     static constexpr size_t SUB_COMMAND_SIZE = 20;
     char command[SUB_COMMAND_SIZE];
