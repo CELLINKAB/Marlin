ab3ce96e8cb28aebc7ec99ea9f82ceb88ebb8447
Skyelar Craver <sc@cellink.com>
2022-12-16 17:06:04 -0500

    ensure crc from byte array matches from bytes
    Signed-off-by: Skyelar Craver <sc@cellink.com>

--- Marlin/src/feature/guppi_printhead/crc_test.cpp
+++ Marlin/src/feature/guppi_printhead/crc_test.cpp
@@ -7,3 +7,9 @@ static constexpr auto crc_correctness_test = []{
     static constexpr uint16_t value = crc16_from_bytes(test_str, 16);
     static_assert(value == 21729, "CRC calculation is incorrect");
 };
+
+static constexpr auto crc_correctness_from_bytes_test = []{
+    static constexpr std::array<uint8_t, 17> test_arr{u8"extrude 20 units"};
+    static constexpr uint16_t crc = calculate_crc16(test_arr);
+    static_assert(crc == 21729, "CRC from bytes is incorrect");
+};
