881956608c624072329b3f80e000949589a187cb
Skyelar Craver <sc@cellink.com>
2022-12-20 11:54:12 -0500

    better static assert, all tests pass
    Signed-off-by: Skyelar Craver <sc@cellink.com>

--- Marlin/src/feature/guppi_printhead/crc_test.cpp
+++ Marlin/src/feature/guppi_printhead/crc_test.cpp
@@ -4,12 +4,25 @@ using namespace printhead;
 
 namespace test {
 
+template<typename T, T val>
+struct _static_assert_val{
+    constexpr _static_assert_val() = default;
+    constexpr _static_assert_val(T v) {};
+};
+
+template<typename T, T A, typename U, U B>
+constexpr void _static_assert_eq(_static_assert_val<T, A>, _static_assert_val<U, B>){
+    static_assert(A == B, "Values not equal!");
+}
+
+#define static_assert_eq(A, B) _static_assert_eq(_static_assert_val<decltype(A), A>{}, _static_assert_val<decltype(B), B>{})
+
 template<typename T, size_t N, size_t M>
-constexpr auto compare_arrays(std::array<T,N> a, std::array<T,M> b) -> bool
+constexpr auto array_compare(const std::array<T,N>& a, const std::array<T,M>& b) -> bool
 {
     bool no_mismatches = true;
-    for (size_t i = 0; i < b.size(); ++i)
-        no_mismatches = no_mismatches && (a[i] == b[i]);
+    for (size_t i = 0; i < M; ++i)
+        no_mismatches &= (a[i]==b[i]);
     return no_mismatches;
 };
 
@@ -20,24 +33,23 @@ void cat_cats()
     static constexpr std::array part_2{4, 5, 6, 7, 8};
     static constexpr std::array part_3{9};
     static constexpr std::array reconstructed = concatenate(part_1, part_2, part_3);
-    static_assert(sizeof(full_arr) == sizeof(reconstructed) && full_arr.size() == reconstructed.size(),
-                  "concatenation yielded length mismatch");
-    static_assert(compare_arrays(full_arr, reconstructed), "concatenation yielded value mismatch");
+    static_assert_eq(full_arr.size(), reconstructed.size());
+    static_assert(array_compare(full_arr, reconstructed));
 }
 
 void byte_array_conversion()
 {
     static constexpr std::array<uint8_t, 4> expected{0x04, 0x03, 0x02, 0x01};
     static constexpr auto from_conversion = byte_array<uint32_t>(0x01020304);
-    static_assert(from_conversion.size() == 4, "conversion from integral to byte array yielded bad length");
-    static_assert(compare_arrays(expected, from_conversion), "bad conversion from integral to bytes");
+    static_assert_eq(from_conversion.size(), 4);
+    static_assert(array_compare(expected, from_conversion));
 }
 
 void crc_correctness()
 {
     static constexpr uint8_t test_str[] = u8"extrude 20 units";
-    static constexpr uint16_t value = crc16_from_bytes(test_str, 16);
-    static_assert(value == 21729, "CRC calculation is incorrect");
+    static constexpr uint16_t value = crc16_from_bytes(test_str, 17);
+    static_assert_eq(value, 35417);
 }
 
 void crc_correctness_from_bytes_matches_raw()
@@ -45,16 +57,17 @@ void crc_correctness_from_bytes_matches_raw()
     static constexpr std::array<uint8_t, 17> test_arr{u8"extrude 20 units"};
     static constexpr uint16_t crc_from_array = calculate_crc16(test_arr);
     static constexpr uint16_t crc_from_data = crc16_from_bytes(test_arr.data(), test_arr.size());
-    static_assert(crc_from_array == crc_from_data, "CRC from bytes is incorrect");
+    static_assert_eq(crc_from_array, crc_from_data);
+    static_assert_eq(crc_from_array, 35417);
 }
 
 void void_packet_bytes()
 {
-    static constexpr std::array<uint8_t, 8> known_good_arr{0x01, 0x00, 0x20, 0x00, 0x00, 0x00, 0x3E, 0x83};
-    static constexpr Packet packet(Index::One, Command::GET_MEASURED_TEMP);
-    static_assert(known_good_arr.size() == packet.bytes().size(), "packet gives bad size arrays");
-    // static_assert(compare_arrays(known_good_arr, packet.header_bytes()), "packet gives bad bytes in the header");
-    // static_assert(compare_arrays(known_good_arr, packet.bytes()), "packet gives bad bytes in the CRC");
+    static constexpr std::array<uint8_t, 8> known_good_arr{0x01, 0x00, 0x20, 0x00, 0x00, 0x00, 0xF4, 0x78};
+    static constexpr Packet packet(Index::Two, Command::GET_MEASURED_TEMP);
+    static_assert_eq(known_good_arr.size(), packet.bytes().size());
+    static_assert(array_compare(known_good_arr, packet.header_bytes()), "packet header doesn't match");
+    static_assert(array_compare(known_good_arr, packet.bytes()), "packet CRC doesn't match");
 }
 
 } // namespace test
