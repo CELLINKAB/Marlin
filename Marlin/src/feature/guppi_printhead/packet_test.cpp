02940fa753f2d3537bb642e28fb5381e449036da
Skyelar Craver <sc@cellink.com>
2023-01-19 10:51:37 -0500

    rename file, move common test utilities to header
    Signed-off-by: Skyelar Craver <sc@cellink.com>

--- Marlin/src/feature/guppi_printhead/packet_test.cpp
+++ Marlin/src/feature/guppi_printhead/packet_test.cpp
@@ -1,30 +1,9 @@
 #include "packet.h"
+#include "test.h"
 
 using namespace printhead;
 
 namespace test {
-
-template<typename T, T val>
-struct _static_assert_val{
-    constexpr _static_assert_val() = default;
-    constexpr _static_assert_val(T v) {};
-};
-
-template<typename T, T A, typename U, U B>
-constexpr void _static_assert_eq(_static_assert_val<T, A>, _static_assert_val<U, B>){
-    static_assert(A == B, "Values not equal!");
-}
-
-#define static_assert_eq(A, B) _static_assert_eq(_static_assert_val<decltype(A), A>{}, _static_assert_val<decltype(B), B>{})
-
-template<typename T, size_t N, size_t M>
-constexpr auto array_compare(const std::array<T,N>& a, const std::array<T,M>& b) -> bool
-{
-    bool no_mismatches = true;
-    for (size_t i = 0; i < M; ++i)
-        no_mismatches &= (a[i]==b[i]);
-    return no_mismatches;
-};
 
 void cat_cats()
 {
