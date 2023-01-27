9a0087b9fff0b11bf9bf9ac3dcabf01b2e4a0bca
Skyelar Craver <sc@cellink.com>
2023-01-27 15:21:30 -0500

    add pullup template argument

--- Marlin/src/feature/door_sensor.h
+++ Marlin/src/feature/door_sensor.h
@@ -1,25 +1,26 @@
 #include "../inc/MarlinConfig.h"
 
-template<pin_t SENSOR>
+template<pin_t SENSOR, bool PULLUP = false>
 struct DoorSensor
 {
     DoorSensor()
     {
-        SET_INPUT_PULLUP(SENSOR);
+        if constexpr (PULLUP) {
+            SET_INPUT_PULLUP(SENSOR);
+        } else {
+            SET_INPUT_PULLDOWN(SENSOR);
+        }
         static auto report = []() {
-                delayMicroseconds(100); // allow signal to settle
-                const bool door_state = READ(SENSOR);
-                SERIAL_ECHOLNPGM("DO:", door_state);
-            };
-        attachInterrupt(
-            SENSOR,
-            static_cast<void (*)()>(report),
-            CHANGE);
+            delayMicroseconds(100); // allow signal to settle
+            const bool door_state = READ(SENSOR);
+            SERIAL_ECHOLNPGM("DO:", door_state);
+        };
+        attachInterrupt(SENSOR, static_cast<void (*)()>(report), CHANGE);
     }
 
-    ~DoorSensor() { detachInterrupt(SENSOR); }
-    DoorSensor(const DoorSensor &) = delete;
-    DoorSensor(DoorSensor &&) = delete;
-    DoorSensor & operator=(const DoorSensor &) = delete;
-    DoorSensor & operator=(DoorSensor &&) = delete;
+    ~DoorSensor() {detachInterrupt(SENSOR);}
+    DoorSensor(const DoorSensor&) = delete;
+    DoorSensor(DoorSensor&&) = delete;
+    DoorSensor& operator=(const DoorSensor&) = delete;
+    DoorSensor& operator=(DoorSensor&&) = delete;
 };
