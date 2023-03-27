70fa3b24e0fdb42254bfc0f4b074ff30d8ed3582
Skyelar Craver <sc@cellink.com>
2023-03-27 15:12:33 -0400

    simplify sanity checks

--- Marlin/src/gcode/cellink/G514_G515.cpp
+++ Marlin/src/gcode/cellink/G514_G515.cpp
@@ -31,7 +31,9 @@ void GcodeSuite::G515()
     static constexpr xy_pos_t GRIPPER_ABSOLUTE_XY{135, -45};
     static constexpr float GRIP_Z_HEIGHT = -5.0f;
     static constexpr float RELEASE_Z_HEIGHT = 10.0f;
-    static constexpr float DETECTION_THRESHOLD = 10.0f;
+    static constexpr float GRIP_THRESHOLD = -20.0f;
+    static constexpr float RELEASE_THRESHOLD = 0.0f;
+    static constexpr float THRESHOLD_HYSTERESIS = 1.0f;
 
     if (homing_needed_error())
         return;
@@ -43,7 +45,6 @@ void GcodeSuite::G515()
 
     bool is_releasing = parser.seen('R');
 
-    float vacuum_baseline = gripper_vacuum.read_avg();
     if (is_releasing) {
         {
             do_blocking_move_to_z(GRIP_Z_HEIGHT);
@@ -52,21 +53,18 @@ void GcodeSuite::G515()
             const millis_t timeout = millis() + 8000;
             current_position.z = RELEASE_Z_HEIGHT;
             planner.buffer_line(current_position, 8.0f);
-            while (millis() < timeout
-                   && gripper_vacuum.read_avg() < 0)
+            while (millis() < timeout && gripper_vacuum.read_avg() < 0)
                 idle();
         }
         planner.synchronize();
         //Expect pressure down
         set_gripper_valves(GripperState::Close);
 
-        float vacuum_delta = gripper_vacuum.read_avg() - vacuum_baseline;
-        if (DEBUGGING(INFO)) {
-            SERIAL_ECHOLNPAIR_F("vacuum_baseline:", vacuum_baseline);
-            SERIAL_ECHOLNPAIR_F("vacuum_delta:", vacuum_delta);
-        }
-        if (vacuum_delta < DETECTION_THRESHOLD) {
+        if (float reading = gripper_vacuum.read();
+            reading < RELEASE_THRESHOLD - THRESHOLD_HYSTERESIS) {
             SERIAL_ECHOLN("RELEASE_FAILED");
+            if (DEBUGGING(INFO))
+                SERIAL_ECHOLNPGM("pressure reading: ", reading);
         }
     } else {
         set_gripper_valves(GripperState::Grip);
@@ -76,14 +74,11 @@ void GcodeSuite::G515()
         set_gripper_valves(GripperState::Close);
         do_blocking_move_to_z(RELEASE_Z_HEIGHT);
         SET_SOFT_ENDSTOP_LOOSE(false);
-        // Expect pressure down (with pump working)
-        float vacuum_delta = vacuum_baseline - gripper_vacuum.read_avg();
-        if (DEBUGGING(INFO)) {
-            SERIAL_ECHOLNPAIR_F("vacuum_baseline:", vacuum_baseline);
-            SERIAL_ECHOLNPAIR_F("vacuum_delta:", vacuum_delta);
-        }
-        if (vacuum_delta < DETECTION_THRESHOLD) {
+
+        if (float reading = gripper_vacuum.read(); reading > GRIP_THRESHOLD - THRESHOLD_HYSTERESIS) {
             SERIAL_ECHOLN("GRIP_FAILED");
+            if (DEBUGGING(INFO))
+                SERIAL_ECHOLNPGM("pressure reading: ", reading);
         }
     }
     do_blocking_move_to_z(Z_AFTER_PROBING);
