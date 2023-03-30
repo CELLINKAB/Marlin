da187fd2c9f8af6222ce827c3a9342d877a68701
Skyelar Craver <sc@cellink.com>
2023-03-30 15:38:40 -0400

    fix regulator constraints

--- Marlin/src/feature/air_system/regulator.h
+++ Marlin/src/feature/air_system/regulator.h
@@ -9,6 +9,7 @@ struct Regulator
 {
     inline void set_point(float kPa)
     {
+        kPa = constrain(kPa, 0.0, REG_MAX);
         uint32_t value = static_cast<uint32_t>(kPa * DAC_FACTOR);
         analogWrite(REG, value);
         pressure = kPa;
@@ -16,8 +17,8 @@ struct Regulator
     inline float set_point() const { return pressure; }
 
 private:
-    // 200kPa regulator 5V analog input clipped to 3.3v, 12 bit DAC
-    static constexpr float DAC_FACTOR = 4096.0 / ((3.3 / 5.0) * 200.0);
+    static constexpr float REG_MAX = (3.3 / 5.0) * 200.0;
+    static constexpr float DAC_FACTOR = 256.0 / REG_MAX;
 
     float pressure;
 };
