a94f0ef65e764dadf6a0076223f232f66171ecf2
Skyelar Craver <sc@cellink.com>
2023-05-02 15:06:55 -0400

    change offset to 0

--- Marlin/src/module/settings.cpp
+++ Marlin/src/module/settings.cpp
@@ -37,7 +37,7 @@
 
 // Change EEPROM version if the structure changes
 #define EEPROM_VERSION "V88"
-#define EEPROM_OFFSET 100
+#define EEPROM_OFFSET 0
 
 // Check the integrity of data offsets.
 // Can be disabled for production build.
