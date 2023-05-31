268c8e71399a1b859b61920a1c58693573772211
Skyelar Craver <sc@cellink.com>
2023-05-31 09:58:56 -0400

    boost homing current, remove hold multiplier

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2715,7 +2715,7 @@
  */
 #if HAS_TRINAMIC_CONFIG || HAS_TMC26X
 
-  #define HOLD_MULTIPLIER    0.5  // Scales down the holding current from run current
+  #define HOLD_MULTIPLIER    1.0  // Scales down the holding current from run current
 
   /**
    * Interpolate microsteps to 256
@@ -2724,8 +2724,8 @@
   #define INTERPOLATE      true
 
   #if AXIS_IS_TMC(X)
-    #define X_CURRENT       800        // (mA) RMS current. Multiply by 1.414 for peak current.
-    #define X_CURRENT_HOME  200        // (mA) RMS current for sensorless homing
+    #define X_CURRENT       700        // (mA) RMS current. Multiply by 1.414 for peak current.
+    #define X_CURRENT_HOME  700        // (mA) RMS current for sensorless homing
     #define X_MICROSTEPS     16        // 0..256
     #define X_RSENSE          .15
     #define X_CHAIN_POS      -1        // -1..0: Not chained. 1: MCU MOSI connected. 2: Next in chain, ...
@@ -2744,8 +2744,8 @@
   #endif
 
   #if AXIS_IS_TMC(Y)
-    #define Y_CURRENT       800
-    #define Y_CURRENT_HOME  200
+    #define Y_CURRENT       700
+    #define Y_CURRENT_HOME  700
     #define Y_MICROSTEPS     16
     #define Y_RSENSE          .15
     #define Y_CHAIN_POS      -1
@@ -2754,8 +2754,8 @@
   #endif
 
   #if AXIS_IS_TMC(Y2)
-    #define Y2_CURRENT      800
-    #define Y2_CURRENT_HOME 200
+    #define Y2_CURRENT      700
+    #define Y2_CURRENT_HOME 700
     #define Y2_MICROSTEPS    16
     #define Y2_RSENSE         .15
     #define Y2_CHAIN_POS     -1
@@ -2764,8 +2764,8 @@
   #endif
 
   #if AXIS_IS_TMC(Z)
-    #define Z_CURRENT       800
-    #define Z_CURRENT_HOME  200
+    #define Z_CURRENT       700
+    #define Z_CURRENT_HOME  700
     #define Z_MICROSTEPS     32
     #define Z_RSENSE          .15
     #define Z_CHAIN_POS      -1
