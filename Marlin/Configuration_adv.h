fe7e58a15bf6e2722f2a433610ccd1c4acebbb0c
Skyelar Craver <sc@cellink.com>
2023-06-12 10:49:17 -0400

    update defaults with newest results

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2725,7 +2725,7 @@
 
   #if AXIS_IS_TMC(X)
     #define X_CURRENT       600        // (mA) RMS current. Multiply by 1.414 for peak current.
-    #define X_CURRENT_HOME  130        // (mA) RMS current for sensorless homing
+    #define X_CURRENT_HOME  110        // (mA) RMS current for sensorless homing
     #define X_MICROSTEPS     4        // 0..256
     #define X_RSENSE          .15
     #define X_CHAIN_POS      -1        // -1..0: Not chained. 1: MCU MOSI connected. 2: Next in chain, ...
@@ -2745,7 +2745,7 @@
 
   #if AXIS_IS_TMC(Y)
     #define Y_CURRENT       600
-    #define Y_CURRENT_HOME  140
+    #define Y_CURRENT_HOME  90
     #define Y_MICROSTEPS     4
     #define Y_RSENSE          .15
     #define Y_CHAIN_POS      -1
@@ -2755,7 +2755,7 @@
 
   #if AXIS_IS_TMC(Y2)
     #define Y2_CURRENT      600
-    #define Y2_CURRENT_HOME 140
+    #define Y2_CURRENT_HOME 90
     #define Y2_MICROSTEPS    4
     #define Y2_RSENSE         .15
     #define Y2_CHAIN_POS     -1
@@ -3164,12 +3164,12 @@
 
   #if EITHER(SENSORLESS_HOMING, SENSORLESS_PROBING)
     // TMC2209: 0...255. TMC2130: -64...63
-    #define X_STALL_SENSITIVITY  70
+    #define X_STALL_SENSITIVITY  35
 
     //#define X2_STALL_SENSITIVITY X_STALL_SENSITIVITY
-    #define Y_STALL_SENSITIVITY  80
+    #define Y_STALL_SENSITIVITY  60
     #define Y2_STALL_SENSITIVITY Y_STALL_SENSITIVITY
-    #define Z_STALL_SENSITIVITY  80
+    #define Z_STALL_SENSITIVITY  60
     //#define Z2_STALL_SENSITIVITY Z_STALL_SENSITIVITY
     //#define Z3_STALL_SENSITIVITY Z_STALL_SENSITIVITY
     //#define Z4_STALL_SENSITIVITY Z_STALL_SENSITIVITY
