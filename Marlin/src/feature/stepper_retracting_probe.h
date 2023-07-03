4b823c0e0ffd2639e00c2c08c9b9dbe3d597a931
Skyelar Craver <sc@cellink.com>
2023-07-03 14:46:57 -0400

    mark const

--- Marlin/src/feature/stepper_retracting_probe.h
+++ Marlin/src/feature/stepper_retracting_probe.h
@@ -59,7 +59,7 @@ struct StepperRetractingProbe
 
     constexpr inline void reset_position() noexcept { state = ProbeState::Unknown; }
 
-    constexpr inline bool is_deployed() noexcept {return state == ProbeState::Deployed;}
+    constexpr inline bool is_deployed() const noexcept {return state == ProbeState::Deployed;}
 
 private:
     using STMC = SimpleTMC<PROBE_EN_PIN, PROBE_STOP_PIN>;
