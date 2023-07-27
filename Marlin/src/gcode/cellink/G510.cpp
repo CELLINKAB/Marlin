cd13323aadc63ad536241b166d159b7a6c9b42cd
Skyelar Craver <sc@cellink.com>
2023-07-27 14:31:46 -0400

    move coordinate system change

--- Marlin/src/gcode/cellink/G510.cpp
+++ Marlin/src/gcode/cellink/G510.cpp
@@ -39,6 +39,9 @@ void GcodeSuite::G510()
         return;
     }
 
+    // use coordinate system to match tool
+    select_coordinate_system(active_extruder);
+
     // clear calibration on current printhead
     if (parser.seen('R')) {
         optical_autocal.reset(active_extruder);
@@ -60,9 +63,6 @@ void GcodeSuite::G510()
     start_pos.z = parser.axisunitsval('Z', AxisEnum::Z_AXIS, DEFAULT_START_POS.z);
 
     const auto feedrate = parser.feedrateval('F', 25.0f);
-
-    // use coordinate system to match tool
-    select_coordinate_system(active_extruder);
 
     if (parser.seen('C')) {
         optical_autocal.calibrate(start_pos, feedrate);
