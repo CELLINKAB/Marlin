c23300df8433a08af7035818e393ce2fb059da42
Skyelar Craver <skyelar.craver@cellink.com>
2023-12-11 14:21:53 -0500

    fix feature gates

--- Marlin/src/gcode/gcode.h
+++ Marlin/src/gcode/gcode.h
@@ -1339,9 +1339,10 @@ private:
     static void M1030();
   #endif
 
-  #if ENABLED(FESTO_PNEUMATICS)
+  #if ALL(FESTO_PNEUMATICS, CELLINK_REPORTING)
     static void M773();
     static void M774();
+    static void M816();
     static void M1036(); // set pressure regulator
     static void M1036_report(bool for_replay);
     static void M1062(); // get pressure sensors
@@ -1395,7 +1396,6 @@ private:
     static void M810();
     static void M811();
     static void M814();
-    static void M816();
     static void M817();
     static void M819();
     static void M822();
